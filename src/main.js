2b416c19b8b6c9df089b6371c839da566f39a9ab
fanguyun214 <289696260@qq.com>
2017-06-10 21:24:36 +0800

    patr1-header组件开发

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
+import 'babel-polyfill'
 import Vue from 'vue'
 import App from './App'
-import router from './router'
+import fastclick from 'fastclick'
 
-Vue.config.productionTip = false
+import 'common/stylus/index.styl'
 
-/* eslint-disable no-new */
+fastclick.attach(document.body)
 new Vue({
   el: '#app',
-  router,
   render: h => h(App)
 })
