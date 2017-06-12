6f2c0c5636b7c982ca181c2778433e8cdb2cb706
fanguyun214 <289696260@qq.com>
2017-06-12 22:31:46 +0800

    part3路由创建

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import 'babel-polyfill'
 import Vue from 'vue'
 import App from './App'
+import router from './router/index'
 import fastclick from 'fastclick'
 
 import 'common/stylus/index.styl'
@@ -8,5 +9,6 @@ import 'common/stylus/index.styl'
 fastclick.attach(document.body)
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
 })
